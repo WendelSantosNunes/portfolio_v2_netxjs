516e38eec4fe465f7bb828739feb9215badaa790
WendelSantosNunes <wendelnunes9999@gmail.com>
2023-09-11 16:00:31 -0300

    refactor: Presentation

--- src/app/components/Presentation/index.tsx
+++ src/app/components/Presentation/index.tsx
@@ -8,14 +8,14 @@ import ImgProgrammng from "../../assets/undraw_programming_re_kg9v 1.svg";
 export function Presentation() {
   return (
     <section className="flex justify-between items-center mt-36 px-4 max-md:justify-center max-md:flex-col-reverse max-w-tela w-full h-full m-auto">
-      <div className="text-white max-md:mt-5 max-w-sm w-full text-center">
+      <div className="text-white max-md:mt-5 max-w-sm w-full max-md:text-center">
         <h2 className="text-3xl mb-5 max-sm:text-2xl">Olá, eu sou o</h2>
         <h1 className="text-color-border-1 text-5xl max-sm:text-4xl">
           Wendel Santos
         </h1>
         <h2 className="text-3xl mt-5 max-sm:text-2xl">Front-End Developer</h2>
 
-        <div className="flex justify-between mt-14 w-72 m-auto">
+        <div className="flex justify-between mt-14 w-72 max-md:mx-auto">
           <a
             href="https://www.linkedin.com/in/wendel-nunes1/"
             target="_blank"
