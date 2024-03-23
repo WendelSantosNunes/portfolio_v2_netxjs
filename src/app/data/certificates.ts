95dcc7f28f18c8fb403296c0bdee18c0429ca9e4
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2024-03-23 19:06:40 -0300

    Update project

--- src/app/data/certificates.ts
+++ src/app/data/certificates.ts
@@ -5,14 +5,66 @@ import RocketseatFundamentar from "../assets/Rocketseat-Fundamentar.png"
 import OrigamidFlexbox from "../assets/Origamid - CSS Flexbox.png"
 import OrigamidWeb from "../assets/Origamid-Web Design Completo.png"
 import aplicacaoJavaSpringBoot from "../assets/Aplicacao-java-spring-boot.png"
+import GitEGithub from '../assets/Curso Git e GitHub_ compartilhando e colaborando em projetos.png'
+import GitEGithub2 from "../assets/Git e Github: Estratégias de Ramificação, Conflitos e Pull Requests.jpeg"
+import modelagem1 from "../assets/Modelagem de banco de dados: Entidades, relacionamentos e atributos.jpeg"
+import modelagem2 from "../assets/Modelagem de banco de dados relacional: modelagem lógica e física.jpeg"
+import sqlComMysql from "../assets/Wendel dos Santos Nunes - Curso SQL com MySQL_ manipule e consulte dados - Alura.png"
+import javaRefatoracao from "../assets/Wendel dos Santos Nunes - Curso Java e refatoração_ melhorando códigos com boas práticas - Alura.png"
+import javaClean from "../assets/Wendel dos Santos Nunes - Curso Java e Clean Architecture_ descomplicando arquitetura de software - Alura.png"
+import java from "../assets/java.jpg"
 
 export const certificatesAll = [
-
    // {
    //    description: 'Curso em andamento: Java COMPLETO 2023 Programação Orientada a Objetos + Projetos',
    //    link: "#",
    //    img: andamento,
-   //  },
+   // },
+   {
+      description: 'Java e refatoração: melhorando códigos com boas práticas',
+      link: "https://cursos.alura.com.br/certificate/7a0d2103-e5ad-4d5d-a3a1-e12489f09eb9?lang=pt_BR",
+      img: javaRefatoracao,
+   },
+   {
+      description: 'Java e Clean Architecture: descomplicando arquitetura de software',
+      link: "https://cursos.alura.com.br/certificate/7979fce3-1eb9-4630-bc79-97d84a9304a0?lang=pt_BR",
+      img: javaClean,
+   },
+   {
+      description: 'SQL com MySQL: manipule e consulte dados',
+      link: "https://cursos.alura.com.br/certificate/1b15bb35-79b4-40b7-94d8-8c9f24b3d570?lang=pt_BR",
+      img: sqlComMysql,
+   },
+   {
+      description: 'Modelagem de banco de dados relacional: álgebra relacional',
+      link: "https://cursos.alura.com.br/certificate/d06182db-5b34-4b2f-8c85-ee1b7277e6fa?lang=pt_BR",
+      img: modelagem2,
+   },
+   {
+      description: 'Modelagem de banco de dados relacional: modelagem lógica e física',
+      link: "https://cursos.alura.com.br/certificate/dcd7ee16-7ca6-4111-a252-7106fb431ceb?lang=pt_BR",
+      img: modelagem2,
+   },
+   {
+      description: 'Modelagem de banco de dados: Entidades, relacionamentos e atributos',
+      link: "https://cursos.alura.com.br/certificate/4bf1a0a5-f38f-474d-a434-8ef5c32dcc52?lang=pt_BR",
+      img: modelagem1,
+   },
+   {
+      description: 'Git e Github: estratégias de ramificação, Conflitos e Pull Requests',
+      link: "https://cursos.alura.com.br/certificate/fe1dde51-7be9-4148-baeb-5908b347a3d3?lang=pt_BR",
+      img: GitEGithub2,
+   },
+   {
+      description: 'Git e GitHub: compartilhando e colaborando em projetos',
+      link: "https://cursos.alura.com.br/certificate/2bbb3c22-7a9b-4fd0-8772-1c2341fe964f?lang=pt_BR",
+      img: GitEGithub,
+   },
+   {
+      description: 'Curso em andamento: Java COMPLETO 2023 Programação Orientada a Objetos + Projetos',
+      link: "http://ude.my/UC-d9083b42-81e7-4591-b693-46b3a7303a88",
+      img: java,
+   },
    {
      description: 'Certificado do curso Criação de uma Aplicação Java com Spring da Rocketseat',
      link: "https://app.rocketseat.com.br/certificates/464df6ed-bae7-48b4-87c2-7e2d76b078ea",
